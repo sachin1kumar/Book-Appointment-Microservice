12ead12f76ed98bb46a7b91af9beb1f28ae1e5d2
Sachin <sachinkmr375@gmail.com>
2020-09-13 11:40:58 +0530

    first commit

---
+++ src/main/java/com/book/appointment/bookappointment/controller/DateTimeConfig.java
@@ -0,0 +1,2 @@
+package com.book.appointment.bookappointment.controller;public class DateTimeConfig {
+}
---
+++ src/main/java/com/book/appointment/bookappointment/model/BookAppointment.java
@@ -0,0 +1,2 @@
+package com.book.appointment.bookappointment.model;public class BookAppointment {
+}
